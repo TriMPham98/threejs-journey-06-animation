b74a2de7da6f92fdcd56ee1ecda6950b483c380a
Tri <trimpham98@gmail.com>
2023-05-25 22:20:52 -0700

    Rotate the camera and focus on mesh

--- src/script.js
+++ src/script.js
@@ -37,8 +37,9 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    mesh.position.x = Math.cos(elapsedTime);
-    mesh.position.y = Math.sin(elapsedTime);
+    camera.position.x = Math.cos(elapsedTime);
+    camera.position.y = Math.sin(elapsedTime);
+    camera.lookAt(mesh.position);
     
     // Render
     renderer.render(scene, camera);
