a43be3cbaef425e0b0c6cce923f5d534bd4afe95
Tri <trimpham98@gmail.com>
2023-05-25 22:37:23 -0700

    Use .to method to move mesh's position to right and back

--- src/script.js
+++ src/script.js
@@ -30,17 +30,20 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-// Clock
-const clock = new THREE.Clock();
+// // Clock
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2});
+gsap.to(mesh.position, {duration: 3, delay: 2, x: 0});
 
 // Animations
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+    // const elapsedTime = clock.getElapsedTime();
 
-    // Update objects
-    camera.position.x = Math.cos(elapsedTime);
-    camera.position.y = Math.sin(elapsedTime);
-    camera.lookAt(mesh.position);
+    // // Update objects
+    // camera.position.x = Math.cos(elapsedTime);
+    // camera.position.y = Math.sin(elapsedTime);
+    // camera.lookAt(mesh.position);
     
     // Render
     renderer.render(scene, camera);
