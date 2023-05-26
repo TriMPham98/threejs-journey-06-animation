37fbc5e368d67b31bd106318681dbcd13b9e19fc
Tri <trimpham98@gmail.com>
2023-05-25 21:46:58 -0700

    Add tick function that recursively calls tick

--- src/script.js
+++ src/script.js
@@ -29,3 +29,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+// Animations
+const tick = () => {
+    console.log('tick');
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
