2d53c541d34b2eb2553ad1a0751ecfaf895075c2
Tri <trimpham98@gmail.com>
2023-05-25 22:09:54 -0700

    Use deltaTime to standardize frame rate animation

--- src/script.js
+++ src/script.js
@@ -29,11 +29,19 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
+// Time 
+let time = Date.now();
+
 // Animations
 const tick = () => {
+    // Time
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
     // Update objects
-    mesh.rotation.x -= 0.01;
-    mesh.rotation.y += 0.01;
+    // mesh.rotation.x -= 0.01;
+    mesh.rotation.y += 0.001 * deltaTime;
 
     // Render
     renderer.render(scene, camera);
